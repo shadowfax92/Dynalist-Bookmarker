b0748373c2b1efba091134803aa6d6478d515e95
nikhil venkat sonti <nikhilsv92@gmail.com>
2019-07-20 18:12:49 +0100

    updating all packages to latest version

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <h2> {{ app_title }} </h2>
+    <form>
+    <label><b>Title</b></label>
+    <input type="text" placeholder="Title of the bookmark" v-model="bookmark_title">
+    <br>
+    <label><b>Tags</b></label>
+    <input type="text" placeholder="Tags. Ex, #read" v-model="bookmark_tags">
+    <br>
+    <label><b>Notes</b></label>
+    <input type="text" placeholder="Any notes" v-model="bookmark_notes">
+    <button :v-on:click="storeData">Store!</button>
+    <br>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      msg: 'Test!'
+  data: {
+    app_title: 'Bookmarker',
+      bookmark_title: '',
+      bookmark_tags: '',
+      bookmark_notes: '',
+  },
+  methods: {
+    storeData: function() {
+      console.log("Storing data for " + bookmark_title);
     }
-  }
+  },
 }
 </script>
 
