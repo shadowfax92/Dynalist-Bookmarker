8393d2bf3516fc1aaa605e906b585e1fb0697fc6
nikhil venkat sonti <nikhilsv92@gmail.com>
2019-07-20 22:25:18 +0100

    basic popup and link capture working

--- src/background/index.js
+++ src/background/index.js
@@ -1,4 +1,13 @@
 // OnInstall handler
 chrome.runtime.onInstalled.addListener(details => {
-  console.log(details)
-})
+  console.log(details);
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(request.action === "store-data") {
+      console.log("storing data: "+ request.data);
+      sendResponse({ action: "saved" });
+    }
+  }
+);
+
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,36 +1,71 @@
 <template>
   <div>
-    <h2> {{ app_title }} </h2>
-    <form>
-    <label><b>Title</b></label>
-    <input type="text" placeholder="Title of the bookmark" v-model="bookmark_title">
-    <br>
-    <label><b>Tags</b></label>
-    <input type="text" placeholder="Tags. Ex, #read" v-model="bookmark_tags">
-    <br>
-    <label><b>Notes</b></label>
-    <input type="text" placeholder="Any notes" v-model="bookmark_notes">
-    <button :v-on:click="storeData">Store!</button>
-    <br>
+    <h2>{{ app_title }}</h2>
+    <form @submit.prevent="onSubmit">
+      <label>
+        <b>Title</b>
+      </label>
+      <input type="text" placeholder="Title of the bookmark" v-model="bookmark_title" />
+      <br />
+      <label>
+        <b>Url</b>
+      </label>
+      <input type="url" placeholder="Url" v-model="bookmark_url" />
+      <br />
+      <label>
+        <b>Tags</b>
+      </label>
+      <input type="text" placeholder="Tags. Ex, #read" v-model="bookmark_tags" />
+      <br />
+      <label>
+        <b>Notes</b>
+      </label>
+      <input type="text" placeholder="Any notes" v-model="bookmark_notes" />
+      <br />
+      <button :v-on:click="onSubmit">Store!</button>
+      <br />
     </form>
   </div>
 </template>
 
 <script>
+import { request } from 'http'
 export default {
-  data: {
-    app_title: 'Bookmarker',
+  data() {
+    return {
+      app_title: 'Bookmarker',
       bookmark_title: '',
+      bookmark_url: '',
       bookmark_tags: '',
       bookmark_notes: '',
+    }
+  },
+  mounted() {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      var activeTab = tabs[0]
+      this.bookmark_title = activeTab.title
+      this.bookmark_url = activeTab.url
+    })
   },
   methods: {
-    storeData: function() {
-      console.log("Storing data for " + bookmark_title);
-    }
+    onSubmit: function() {
+      console.log('Storing data for ' + this.bookmark_title)
+      let new_bookmark = {
+        title: this.bookmark_title,
+        tags: this.bookmark_tags,
+        notes: this.bookmark_notes,
+        url: this.bookmark_url,
+      }
+
+      chrome.runtime.sendMessage(
+        { action: 'store-data', data: new_bookmark },
+        response => {
+          console.log(request.action)
+        }
+      )
+    },
   },
 }
 </script>
 
-<style lang="stylus">
-</style>
+<style lang="stylus"></style>
