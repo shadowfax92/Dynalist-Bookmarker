94a5536ee46cc579c62217727d0d200a85d62bbf
nikhil venkat sonti <nikhilsv92@gmail.com>
2019-07-24 20:49:43 +0100

    New CSS, feature - Support of saving popup context.

--- src/background/index.js
+++ src/background/index.js
@@ -6,11 +6,47 @@ chrome.runtime.onInstalled.addListener(details => {
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if(request.action === "store-data") {
-      console.log("storing data: "+ request.data);
+  switch (request.action) {
+    case "send-to-dynalist":
+      console.log("storing data: " + request.data);
       addToBookmarks(request.data);
-      sendResponse({ action: "saved" });
-    }
+      // response is not sent back to popup.js as it's not doesn't have a tab-id.
+      // you need a listener in popup.js to get data.
+      // sendResponse({ action: "saved" });
+      break;
+    case "session-store-set-data":
+      sessionStorageSetItem(request.key, request.value);
+      break;
+    case "session-store-get-data":
+      let key = request.key;
+      
+      SessionStorageGetItem(request.key, (value) => {
+        sendResponse({ action:'', value: value[key] });
+        sendMessageToPopup(sender, { action:'response-session-store-get-data', value: value[key] });
+      });
+      break;
+    default:
+      break;
   }
+}
 );
 
+function sendMessageToPopup(tab_id, value) {
+  chrome.runtime.sendMessage(value, (response) => {
+    console.log("sendMessageToPopup: response = " + response);
+  });
+}
+
+function sessionStorageSetItem(key, value) {
+  // window.sessionStorage.setItem(key, value)
+  let obj = {};
+  obj[key] = value;
+  chrome.storage.local.set(obj);
+};
+
+function SessionStorageGetItem(key, callback) {
+  // let value = window.sessionStorage.getItem(key);
+  chrome.storage.local.get(key, (value) => {
+    callback(value)
+  });
+};
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -3,32 +3,48 @@
     <div>
       <h2 class="header">{{ app_title }}</h2>
     </div>
-    
+
     <div class="container">
       <div>
         <div>
-          <label class="rows">
-            Title
-          </label>
-          <input class="rows" type="text" placeholder="Title of the bookmark" v-model="bookmark_title" />
+          <label class="rows">Title</label>
+          <input
+            class="rows"
+            type="text"
+            placeholder="Title of the bookmark"
+            v-model="bookmark_title"
+            v-on:change="onChange()"
+          />
         </div>
         <div>
-          <label class="rows">
-            Url
-          </label>
-          <input class="rows" type="url" placeholder="Url" v-model="bookmark_url" />
+          <label class="rows">Url</label>
+          <input
+            class="rows"
+            type="url"
+            placeholder="Url"
+            v-model="bookmark_url"
+            v-on:change="onChange()"
+          />
         </div>
         <div>
-          <label class="rows">
-            Tags
-          </label>
-          <input class="rows" type="text" placeholder="Tags. Ex, #read" v-model="bookmark_tags" />
+          <label class="rows">Tags</label>
+          <input
+            class="rows"
+            type="text"
+            placeholder="Tags. Ex, #read"
+            v-model="bookmark_tags"
+            v-on:change="onChange()"
+          />
         </div>
         <div>
-          <label class="rows">
-            Notes
-          </label>
-          <textarea class="rows"  type="text" placeholder="Any notes" v-model="bookmark_notes"></textarea>
+          <label class="rows">Notes</label>
+          <textarea
+            class="rows"
+            type="text"
+            placeholder="Any notes"
+            v-model="bookmark_notes"
+            v-on:change="onChange()"
+          ></textarea>
         </div>
       </div>
       <div class="button-container">
@@ -41,6 +57,7 @@
 
 <script>
 import { request } from 'http'
+
 export default {
   data() {
     return {
@@ -49,6 +66,7 @@ export default {
       bookmark_url: '',
       bookmark_tags: '',
       bookmark_notes: '',
+      page_url: '',
     }
   },
   mounted() {
@@ -56,32 +74,72 @@ export default {
       var activeTab = tabs[0]
       this.bookmark_title = activeTab.title
       this.bookmark_url = activeTab.url
+      this.page_url = activeTab.url
+
+      chrome.runtime.sendMessage({ action: 'session-store-get-data', key: this.page_url }, (response) => {});
+      chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (true) {
+          console.log(sender);
+          console.log('received response-session-store-get-data')
+          if (message.action == 'response-session-store-get-data' && message.value !== undefined) {
+            console.log(message.value);
+            this.setPageData(message.value);
+          }
+        }
+      });
     })
   },
   methods: {
     onSubmit: function() {
-      console.log('Storing data for ' + this.bookmark_title)
-      let new_bookmark = {
-        title: this.bookmark_title,
-        tags: this.bookmark_tags,
-        notes: this.bookmark_notes,
-        url: this.bookmark_url,
-      }
-
       chrome.runtime.sendMessage(
-        { action: 'store-data', data: new_bookmark },
+        { action: 'send-to-dynalist', data: this.getPageData() },
         response => {
           console.log(request.action)
           this.closePopup()
         }
       )
     },
+    getPageData: function() {
+      let data = {
+        title: this.bookmark_title,
+        tags: this.bookmark_tags,
+        notes: this.bookmark_notes,
+        url: this.bookmark_url,
+      }
+      return data
+    },
+    setPageData: function(data) {
+      if (data.title != '') {
+        this.bookmark_title = data.title
+      }
+      if (data.url != '') {
+        this.bookmark_url = data.url
+      }
+      if (data.tags != '') {
+        this.bookmark_tags = data.tags
+      }
+      if (data.notes != '') {
+        this.bookmark_notes = data.notes
+      }
+    },
     onCancel: function() {
       this.closePopup()
     },
     closePopup: function() {
       window.close()
-    }
+    },
+    onChange: function() {
+      chrome.runtime.sendMessage(
+        {
+          action: 'session-store-set-data',
+          key: this.page_url,
+          value: this.getPageData(),
+        },
+        response => {
+          console.log(request.action)
+        }
+      )
+    },
   },
 }
 </script>
@@ -105,7 +163,7 @@ body {
   text-align: center;
   margin-top: 10px;
   border: 1px solid #79bcff;
-  padding: 5px; 
+  padding: 5px;
   border-radius: 5px;
 }
 
@@ -116,7 +174,8 @@ body {
   text-align: left;
 }
 
-div > input, div > textarea {
+div > input,
+div > textarea {
   width: 100%;
   height: 40px;
   border-color: #79bcff;
@@ -124,7 +183,8 @@ div > input, div > textarea {
   border-width: 1px;
 }
 
-input:hover, textarea:hover{
+input:hover,
+textarea:hover {
   background: #f7ffff;
 }
 
@@ -142,77 +202,97 @@ input {
 
 // generated using - https://www.bestcssbuttongenerator.com/#/1
 .myButtonSave {
-	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #79bbff), color-stop(1, #378de5));
-	background:-moz-linear-gradient(top, #79bbff 5%, #378de5 100%);
-	background:-webkit-linear-gradient(top, #79bbff 5%, #378de5 100%);
-	background:-o-linear-gradient(top, #79bbff 5%, #378de5 100%);
-	background:-ms-linear-gradient(top, #79bbff 5%, #378de5 100%);
-	background:linear-gradient(to bottom, #79bbff 5%, #378de5 100%);
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#79bbff', endColorstr='#378de5',GradientType=0);
-	background-color:#79bbff;
-	-moz-border-radius:15px;
-	-webkit-border-radius:15px;
-	border-radius:15px;
-	display:inline-block;
-	cursor:pointer;
-	color:#ffffff;
-	font-family:Arial;
-	font-size:12px;
-	padding:8px 20px;
-	text-decoration:none;
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #79bbff),
+    color-stop(1, #378de5)
+  );
+  background: -moz-linear-gradient(top, #79bbff 5%, #378de5 100%);
+  background: -webkit-linear-gradient(top, #79bbff 5%, #378de5 100%);
+  background: -o-linear-gradient(top, #79bbff 5%, #378de5 100%);
+  background: -ms-linear-gradient(top, #79bbff 5%, #378de5 100%);
+  background: linear-gradient(to bottom, #79bbff 5%, #378de5 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#79bbff', endColorstr='#378de5',GradientType=0);
+  background-color: #79bbff;
+  -moz-border-radius: 15px;
+  -webkit-border-radius: 15px;
+  border-radius: 15px;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 12px;
+  padding: 8px 20px;
+  text-decoration: none;
 }
 .myButtonSave:hover {
-	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #378de5), color-stop(1, #79bbff));
-	background:-moz-linear-gradient(top, #378de5 5%, #79bbff 100%);
-	background:-webkit-linear-gradient(top, #378de5 5%, #79bbff 100%);
-	background:-o-linear-gradient(top, #378de5 5%, #79bbff 100%);
-	background:-ms-linear-gradient(top, #378de5 5%, #79bbff 100%);
-	background:linear-gradient(to bottom, #378de5 5%, #79bbff 100%);
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#378de5', endColorstr='#79bbff',GradientType=0);
-	background-color:#378de5;
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #378de5),
+    color-stop(1, #79bbff)
+  );
+  background: -moz-linear-gradient(top, #378de5 5%, #79bbff 100%);
+  background: -webkit-linear-gradient(top, #378de5 5%, #79bbff 100%);
+  background: -o-linear-gradient(top, #378de5 5%, #79bbff 100%);
+  background: -ms-linear-gradient(top, #378de5 5%, #79bbff 100%);
+  background: linear-gradient(to bottom, #378de5 5%, #79bbff 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#378de5', endColorstr='#79bbff',GradientType=0);
+  background-color: #378de5;
 }
 .myButtonSave:active {
-	position:relative;
-	top:1px;
+  position: relative;
+  top: 1px;
 }
-
 
 .myButtonCancel {
-	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f9f9f9), color-stop(1, #e9e9e9));
-	background:-moz-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-	background:-webkit-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-	background:-o-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-	background:-ms-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-	background:linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#f9f9f9', endColorstr='#e9e9e9',GradientType=0);
-	background-color:#f9f9f9;
-	-moz-border-radius:15px;
-	-webkit-border-radius:15px;
-	border-radius:15px;
-	display:inline-block;
-	cursor:pointer;
-	color:#666666;
-	font-family:Arial;
-	font-size:12px;
-	padding:8px 20px;
-	text-decoration:none;
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #f9f9f9),
+    color-stop(1, #e9e9e9)
+  );
+  background: -moz-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: -webkit-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: -o-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: -ms-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f9f9f9', endColorstr='#e9e9e9',GradientType=0);
+  background-color: #f9f9f9;
+  -moz-border-radius: 15px;
+  -webkit-border-radius: 15px;
+  border-radius: 15px;
+  display: inline-block;
+  cursor: pointer;
+  color: #666666;
+  font-family: Arial;
+  font-size: 12px;
+  padding: 8px 20px;
+  text-decoration: none;
 }
 .myButtonCancel:hover {
-	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #e9e9e9), color-stop(1, #f9f9f9));
-	background:-moz-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-	background:-webkit-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-	background:-o-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-	background:-ms-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-	background:linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#e9e9e9', endColorstr='#f9f9f9',GradientType=0);
-	background-color:#e9e9e9;
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #e9e9e9),
+    color-stop(1, #f9f9f9)
+  );
+  background: -moz-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: -webkit-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: -o-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: -ms-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#e9e9e9', endColorstr='#f9f9f9',GradientType=0);
+  background-color: #e9e9e9;
 }
 .myButtonCancel:active {
-	position:relative;
-	top:1px;
+  position: relative;
+  top: 1px;
 }
-
-
-
 </style>
 
