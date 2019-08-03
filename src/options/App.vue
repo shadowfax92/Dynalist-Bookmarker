83b36117d430e28c3a5d4eaa1ccd420b95b71ee4
nikhil venkat sonti <nikhilsv92@gmail.com>
2019-08-03 15:37:28 +0100

    [feature] options showing dynalist api ui

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <div class="container">
+      <div class="api-box">
+        <label class="rows">Step-1: Paste Dynalist Token</label>
+        <span class="rows">Please copy and paste the token from <a href="https://dynalist.io/developer">dynalist link.</a></span>
+          <input
+            class="rows"
+            type="text"
+            placeholder="Paste the API token"
+          />
+        <span class="rows"><b class="privacy">PRIVACY POLICY: </b>Note, the token is never upload and is only accessible on this browser.</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,11 +19,67 @@
 export default {
   data() {
     return {
-      msg: 'Welcome!'
+
     }
   }
 }
 </script>
 
-<style lang="stylus">
+<style lang="scss">
+body {
+  text-align: center;
+  font-size: 12px;
+  width: 400px;
+  height: auto;
+}
+
+.container {
+  width: 95%;
+  margin: 5px;
+}
+
+.header {
+  display: inline-flex;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #79bcff;
+  padding: 5px;
+  border-radius: 5px;
+}
+
+.rows {
+  display: block;
+  width: 100%;
+  margin-top: 5px;
+  text-align: left;
+}
+
+div > input,
+div > textarea {
+  width: 100%;
+  height: 40px;
+  border: 0;
+  outline: 0;
+  background: transparent;
+  border-bottom: 1px solid #79bcff;
+}
+
+.privacy {
+  color: red;
+}
+
+input:hover,
+textarea:hover {
+  background: #f7ffff;
+}
+
+label {
+  text-transform: uppercase;
+}
+
+input {
+  width: 100%;
+}
 </style>
