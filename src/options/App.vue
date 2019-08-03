150dffd7030e0b69fc2a835d01d35791b1f6e409
nikhil venkat sonti <nikhilsv92@gmail.com>
2019-08-03 22:23:37 +0100

    [css] save and cancel button added

--- src/options/App.vue
+++ src/options/App.vue
@@ -46,6 +46,10 @@
         </select>
       </div>
     </div>
+    <div class="button-container box" v-if="defaultBookmarkLocationSelected && isValidToken">
+      <button class="myButtonSave" v-on:click="onSave">Save</button>
+      <button class="myButtonCancel" v-on:click="onCancel">Cancel</button>
+    </div>
   </div>
 </template>
 
@@ -60,6 +64,7 @@ export default {
       api_token: '',
       isValidToken: undefined,
       showTokenResponse: false,
+      defaultBookmarkLocationSelected: false,
       options: [],
     }
   },
@@ -75,6 +80,8 @@ export default {
         }
       })
     },
+    onSave: function() {},
+    onCancel: function() {},
     extractDynalistDocuments: function(dynalist_nodes) {
       console.log(dynalist_nodes)
       let documents = []
@@ -165,5 +172,106 @@ label {
 
 input {
   width: 100%;
+}
+
+// generated using - https://www.bestcssbuttongenerator.com/#/1
+
+.button-container {
+  margin-top: 10px;
+  text-align: center;
+}
+
+.myButtonSave {
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #79bbff),
+    color-stop(1, #378de5)
+  );
+  background: -moz-linear-gradient(top, #79bbff 5%, #378de5 100%);
+  background: -webkit-linear-gradient(top, #79bbff 5%, #378de5 100%);
+  background: -o-linear-gradient(top, #79bbff 5%, #378de5 100%);
+  background: -ms-linear-gradient(top, #79bbff 5%, #378de5 100%);
+  background: linear-gradient(to bottom, #79bbff 5%, #378de5 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#79bbff', endColorstr='#378de5',GradientType=0);
+  background-color: #79bbff;
+  -moz-border-radius: 15px;
+  -webkit-border-radius: 15px;
+  border-radius: 15px;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 12px;
+  padding: 8px 20px;
+  text-decoration: none;
+}
+.myButtonSave:hover {
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #378de5),
+    color-stop(1, #79bbff)
+  );
+  background: -moz-linear-gradient(top, #378de5 5%, #79bbff 100%);
+  background: -webkit-linear-gradient(top, #378de5 5%, #79bbff 100%);
+  background: -o-linear-gradient(top, #378de5 5%, #79bbff 100%);
+  background: -ms-linear-gradient(top, #378de5 5%, #79bbff 100%);
+  background: linear-gradient(to bottom, #378de5 5%, #79bbff 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#378de5', endColorstr='#79bbff',GradientType=0);
+  background-color: #378de5;
+}
+.myButtonSave:active {
+  position: relative;
+  top: 1px;
+}
+
+.myButtonCancel {
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #f9f9f9),
+    color-stop(1, #e9e9e9)
+  );
+  background: -moz-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: -webkit-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: -o-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: -ms-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f9f9f9', endColorstr='#e9e9e9',GradientType=0);
+  background-color: #f9f9f9;
+  -moz-border-radius: 15px;
+  -webkit-border-radius: 15px;
+  border-radius: 15px;
+  display: inline-block;
+  cursor: pointer;
+  color: #666666;
+  font-family: Arial;
+  font-size: 12px;
+  padding: 8px 20px;
+  text-decoration: none;
+}
+.myButtonCancel:hover {
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #e9e9e9),
+    color-stop(1, #f9f9f9)
+  );
+  background: -moz-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: -webkit-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: -o-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: -ms-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#e9e9e9', endColorstr='#f9f9f9',GradientType=0);
+  background-color: #e9e9e9;
+}
+.myButtonCancel:active {
+  position: relative;
+  top: 1px;
 }
 </style>
