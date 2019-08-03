02f86f22ba6cc9fc08a04915be69a05bd3bb3b9f
nikhil venkat sonti <nikhilsv92@gmail.com>
2019-08-03 19:59:30 +0100

    [api] dynalist list api

--- src/utils/dynalist.js
+++ src/utils/dynalist.js
@@ -1,9 +1,17 @@
 const axios = require('axios');
 
+const DYNALIST_APIS = {
+    edit: 'https://dynalist.io/api/v1/doc/edit',
+    list: 'https://dynalist.io/api/v1/file/list',
+}
 
-const API_TOKEN = 'BsECT6ckocUphrQFvDo6_H1WKa-5Ggy1DE1zq1kRMtRfZ6aen0DJAYH9DPzqjYXn9dlZZNkDA9FIbQTkMJCGuFJzKKaeWzSy3GBhDy3lmNbGnW8-0VZ9dI0KQaDZd2mS'
+const DYNALIST_CONFIG = {
+    note_id: 'jkKwmcyV5dt_fT1LLPRKShzT',
+    api_token: 'BsECT6ckocUphrQFvDo6_H1WKa-5Ggy1DE1zq1kRMtRfZ6aen0DJAYH9DPzqjYXn9dlZZNkDA9FIbQTkMJCGuFJzKKaeWzSy3GBhDy3lmNbGnW8-0VZ9dI0KQaDZd2mS'
 
-export function addToBookmarks(bookmarks_object, callback) {
+}
+
+const addToBookmarks = (bookmarks_object, callback) => {
     let dynalist_title = '[' + bookmarks_object.title + '](' + bookmarks_object.url + ')'
     let dynalist_note = '';
     if (bookmarks_object.tags !== '') {
@@ -14,15 +22,17 @@ export function addToBookmarks(bookmarks_object, callback) {
         dynalist_note = bookmarks_object.notes
     }
 
-    let bookmarks_file_id = 'jkKwmcyV5dt_fT1LLPRKShzT'
     let chrome_bookmarks_id = 'root'
-    dynalistInsert(bookmarks_file_id, chrome_bookmarks_id, dynalist_title, dynalist_note, callback)
+    dynalist_insert_api(DYNALIST_CONFIG.note_id, chrome_bookmarks_id, dynalist_title, dynalist_note, callback)
 }
 
-function dynalistInsert(file_id, parent_id, title, note, callback) {
-    let dynalistInsertDocApi = 'https://dynalist.io/api/v1/doc/edit'
+const validateToken = (api_token, callback)=> {
+    dynalist_list_api(api_token, callback)
+}
+
+const dynalist_insert_api = (file_id, parent_id, title, note, callback) => {
     let body = {
-        'token': API_TOKEN,
+        'token': DYNALIST_CONFIG.api_token,
         'file_id': file_id,
         'changes': [
             {
@@ -35,7 +45,7 @@ function dynalistInsert(file_id, parent_id, title, note, callback) {
             }
         ]
     }
-    axios.post(dynalistInsertDocApi, body).then(function (response) {
+    axios.post(DYNALIST_APIS.edit, body).then(function (response) {
         let status = {}
         if (response._code == 'Ok') {
             status = { success: true }
@@ -47,4 +57,28 @@ function dynalistInsert(file_id, parent_id, title, note, callback) {
             callback(status)
         }
     })
+}
+
+const dynalist_list_api = (api_token, callback) => {
+    let body = {
+        'token': api_token
+    }
+    axios.post(DYNALIST_APIS.list, body).then((response) => {
+        let status = {}
+        if (response._code == 'Ok') {
+            status = { success: true, value: response }
+        }
+        else {
+            status = { success: false }
+        }
+
+        if (callback !== undefined) {
+            callback(status)
+        }
+    })
+}
+
+export {
+    addToBookmarks,
+    validateToken
 }
